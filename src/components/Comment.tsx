6c695b96dea453d314d433b83f9d6466dfeaa777
Guilherme Totti <dev.guilhermetotti@gmail.com>
2024-06-04 08:34:21 -0300

    first commit

---
+++ src/components/Comment.tsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import styles from "./Comment.module.css";
+import { ThumbsUp, Trash } from "phosphor-react";
+import { Avatar } from "./Avatar";
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1;
+    });
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/guitotti.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Guilherme Totti</strong>
+              <time title="14 de Maio às 20:30h" dateTime="2024-05-14 20:30:00">
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button onClick={handleDeleteComment} title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
